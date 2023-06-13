6233a0ad0b2b55d1cdd89afbf37ecf69099b68dd
afornara <a.fornara@cern.ch>
2023-06-13 17:20:24 +0200

    reverted

--- noise_master_study/scans/example_HL_tunescan/base_collider/xtrack_0001/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /afs/cern.ch/work/a/afornara/public/xmask/example_DA_study/noise_master_study/../miniconda/bin/activate
-cd /afs/cern.ch/work/a/afornara/public/xmask/example_DA_study/noise_master_study/scans/example_HL_tunescan/base_collider/xtrack_0001
-python 2_configure_and_track.py > output.txt 2> error.txt
-rm -rf final_* modules optics_repository optics_toolkit tools tracking_tools temp mad_collider.log __pycache__ twiss* errors fc* optics_orbit_at*
